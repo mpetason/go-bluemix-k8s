4980dd74f22984b9ad8fd20098ced18ecb45551b
Michael Petersen <mpetason@gmail.com>
2019-06-05 12:36:09 -0700

    sorted and changed output settings

--- bluemix/get_cluster_owners.go
+++ bluemix/get_cluster_owners.go
@@ -77,7 +77,9 @@ func main() {
 	} else {
 		kubernetesRegions = []string{c.Region}
 	}
-	clusterCount := 0
+
+	allClusters := make(map[string][]string)
+
 	for _, r := range kubernetesRegions {
 		target := v1.ClusterTargetHeader{
 			OrgID:     myorg.GUID,
@@ -90,18 +92,27 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		clustersAPI := clusterClient.Clusters()
 
 		out, err := clustersAPI.List(target)
 		if err != nil {
 			log.Fatal(err)
 		}
-		sort.Slice(out, func(i, j int) bool {
-			return out[i].OwnerEmail < out[j].OwnerEmail
-		})
+
 		for _, c := range out {
-			clusterCount++
-			fmt.Println(clusterCount, "[", c.DataCenter, "]", "[", c.OwnerEmail, "]", "[", c.Name, "]")
+			allClusters[c.OwnerEmail] = append(allClusters[c.OwnerEmail], c.Name)
+			//	fmt.Println(clusterCount, "[", c.DataCenter, "]", "[", c.OwnerEmail, "]", "[", c.Name, "]")
 		}
+	}
+
+	keys := make([]string, 0, len(allClusters))
+	for k := range allClusters {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Println("[", len(allClusters[k]), "]", "[", k, "]", allClusters[k])
 	}
 }
